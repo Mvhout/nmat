7d1a305163e8e2969800cf2b0b9b6de9aecbf646
Mvhout <82750089+Mvhout@users.noreply.github.com>
2021-04-21 02:32:28 +0200

    SYN-CON scan met threading
    Connect scan volledig herschreven met threading. (alleen wat bugs/checks moeten nog geschreven worden)

--- TCP-Connect.py
+++ TCP-Connect.py
@@ -1,28 +1,23 @@
-from socket import *
-import time
-startTime = time.time()
+import socket
+import threading
+import concurrent.futures
 
-print("Toets hieronder de website in welke u gescand wilt hebben zonder http(s):// \n")
-print("Bijvoorbeeld: \n\n google.nl \n spele.nl \n scanme.nmap.org \n")
+print_lock = threading.Lock()
 
-if __name__ == "__main__":
-   target = str(input("Vul hier de website in: \n"))
-   t_IP = gethostbyname(target)
-   print("\nVul hieronder uw begin en eind port in. \nWilt u 1 port scannen, gebruik 1 port hoger bij uw eindport \n")
-   print("Voorbeeld:\n\n2 porten: \nBegin port 80\nEindport 200\n\n1 port:\nBegin port 80\nEind port 81\n")
-   port_a = int(input("Vul uw begin port in om te scannen: "))
-   port_b = int(input("Vul uw eind port in om te scannen: "))
-   print (f"\nStart scan op host: {target} \nop IP adres: {t_IP}\n")
-   
-   for i in range(port_a, port_b + 1):
-      s = socket(AF_INET, SOCK_STREAM)
-     
-      conn = s.connect_ex((t_IP, i))
-      if(conn == 0) :
-        print ('Port %d: OPEN' % (i,))
-      elif(conn != 1):
-      	print ('Port %d: DICHT' % (i,))
-      s.close()
+ip = input("Toets hier uw website in: ")
+porta = int(input("Toets hier uw begin port in: "))
+portb = int(input("Toets hier uw eind port in: "))
 
-tijd = time.time() - startTime
-print(f"\nHet duurde: {tijd} seconden om deze website te scannen")
+def scan(ip,port):
+   scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   scanner.settimeout(1)
+   try:
+      scanner.connect((ip,port))
+      scanner.close()
+      print(f"{port} Open")
+   except:
+      pass
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executer:
+   for port in range(porta,portb + 1):
+      executer.submit(scan,ip,port)
