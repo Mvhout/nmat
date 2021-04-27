92111d4ae4516193914ca02650a2a95903d6be34
Mvhout <82750089+Mvhout@users.noreply.github.com>
2021-04-27 17:09:40 +0200

    volledig werkend

--- nmap(eindopdracht mod4).py
+++ nmap(eindopdracht mod4).py
@@ -1,5 +1,6 @@
 import pyfiglet
 import socket
+from scapy.all import *
 import threading
 import concurrent.futures
 import json
@@ -8,7 +9,7 @@ import csv
 import pandas as pd
 import sys
 import subprocess
-from scapy.all import *
+import xml.etree.cElementTree as ET
 
 
 openp = []
@@ -26,11 +27,12 @@ print ("U kunt kiezen uit 4 verschillende scans: ")
 print ("\n 1. TCP-Connect \n 2. TCP-SYN \n 3. UDP \n 4. XMAS \n" )
 print ("Type het nummer in voor de scan die u wilt uitvoeren \n")
 
-scan = 0
-while scan != "1" and scan != "2" and scan != "3" and scan != "4":
-    scan = input("Vul uw keuze in: ")
+scantype = 0
+while scantype != "1" and scantype != "2" and scantype != "3" and scantype != "4":
+    scantype = input("Vul uw keuze in: ")
 
 # Gebruiker input
+
 ip = input("Toets hier uw website in: ")
 porta = int(input("Toets hier uw begin port in: "))
 portb = int(input("Toets hier uw eind port in: "))
@@ -49,7 +51,8 @@ def portcheck(porta,portb):
 portcheck(porta,portb)
 
 # TCP-connect scan
-if scan == "1":
+if scantype == "1":
+    scantype = "TCP-Connect"
     print_lock = threading.Lock()
     # scan/socket
     def scan(ip, port):
@@ -75,23 +78,24 @@ openp.sort()			#Poorten worden door elkaar in de lijst opgeslagen, vandaar dat i
 geslotenp.sort()
 
 # TCP-SYN scan
-if scan == "2":
-	def scan():
-		for x in range(porta, portb + 1):
-			print(f"Scanning port {x}:")
-			stealth_scan_resp = sr1(IP(dst=ip) / TCP(sport=x, dport=x, flags="S"), timeout=10, verbose=False)
-			if (stealth_scan_resp and stealth_scan_resp.haslayer(TCP)):
-				if (stealth_scan_resp.getlayer(TCP).flags == 0x12):
-					send_rst = sr(IP(dst=ip) / TCP(sport=x, dport=x, flags="R"), timeout=10, verbose=False)
-					print(f"port {x} is Open")
-					openp.append(x)
-			else:
-				geslotenp.append(x)
-				print(f"port {x} is Dicht")
-	scan()
+if scantype == "2":
+    scantype = "TCP-SYN"
+    def scan():
+        for x in range(porta, portb + 1):
+            stealth_scan_resp = sr1(IP(dst=ip) / TCP(sport=x, dport=x, flags="S"), timeout=10, verbose=False)
+            if (stealth_scan_resp and stealth_scan_resp.haslayer(TCP)):
+                if (stealth_scan_resp.getlayer(TCP).flags == 0x12):
+                    send_rst = sr(IP(dst=ip) / TCP(sport=x, dport=x, flags="R"), timeout=10, verbose=False)
+                    print(f"port {x}: Open")
+                    openp.append(x)
+            else:
+                geslotenp.append(x)
+                print(f"port {x}: Dicht")
+    scan()
 
 # UDP scan
-if scan == "3":
+if scantype == "3":
+    scantype = "UDP scan"
     def getServiceName(port, proto):
         try:
             name = socket.getservbyport(int(port), proto)
@@ -122,7 +126,7 @@ if scan == "3":
                 serv = getServiceName(x, 'udp')
                 if not serv:
                     geslotenp.append(x)
-                    print(f"Port {x}: Closed")
+                    print(f"Port {x}: Dicht")
                 else:
                     filteredp.append(x)
                     print(f"Port {x}: Open | Filtered")
@@ -135,24 +139,25 @@ if scan == "3":
     scan()
 
 # XMAS scan
-if scan == "4":
-	for x in range(porta, portb + 1):
-		xmas_scan_resp = sr1(IP(dst=ip) / TCP(dport=x, flags="FPU"), timeout=5, verbose=False)
-		if (str(type(xmas_scan_resp)) == "<type 'NoneType'>"):
-			filteredp.append(x)
-			print("Open|Filtered")
-		elif (xmas_scan_resp and xmas_scan_resp.haslayer(TCP)):
-			if (xmas_scan_resp and xmas_scan_resp.getlayer(TCP).flags == 0x14):
-				geslotenp.append(x)
-				print("Closed")
-		elif (xmas_scan_resp and xmas_scan_resp.haslayer(ICMP)):
-			if (int(xmas_scan_resp.getlayer(ICMP).type) == 3 and int(xmas_scan_resp.getlayer(ICMP).code) in [1, 2, 3, 9,
-																											 10, 13]):
-				filteredp.append(x)
-				print("Filtered")
-		else:
-			filteredp.append(x)
-			print("Port Open | Filtered")
+if scantype == "4":
+    scantype = "XMAS scan"
+    for x in range(porta, portb + 1):
+        xmas_scan_resp = sr1(IP(dst=ip) / TCP(dport=x, flags="FPU"), timeout=5, verbose=False)
+        if (str(type(xmas_scan_resp)) == "<type 'NoneType'>"):
+            filteredp.append(x)
+            print("Open|Filtered")
+        elif (xmas_scan_resp and xmas_scan_resp.haslayer(TCP)):
+            if (xmas_scan_resp and xmas_scan_resp.getlayer(TCP).flags == 0x14):
+                geslotenp.append(x)
+                print(f" Port {x}: Dicht")
+        elif (xmas_scan_resp and xmas_scan_resp.haslayer(ICMP)):
+            if (int(xmas_scan_resp.getlayer(ICMP).type) == 3 and int(xmas_scan_resp.getlayer(ICMP).code) in [1, 2, 3, 9,
+                                                                                                             10, 13]):
+                filteredp.append(x)
+                print(f" Port {x}: Filtered")
+        else:
+            filteredp.append(x)
+            print(f"Port {x}: Open | Filtered")
 
 # Wegschrijven XML/JSON
 
@@ -171,26 +176,28 @@ def wegschrijven():
          doc = ET.SubElement(root, "doc")
 
          ET.SubElement(doc, "field1", name="Target").text = ip
-         ET.SubElement(doc, "field2", name="Typescan").text = scan
-         ET.SubElement(doc, "field3", name="Open").text = str(openp)
-         ET.SubElement(doc, "field4", name="Gesloten").text = str(geslotenp)
-         ET.SubElement(doc, "field5", name="Filtered").text = str(filteredp)
+         ET.SubElement(doc, "field2", name="Typescan").text = scantype
+         ET.SubElement(doc, "field3", name="Port Range").text = f"{porta}-{portb}"
+         ET.SubElement(doc, "field4", name="Open").text = str(openp)
+         ET.SubElement(doc, "field5", name="Gesloten").text = str(geslotenp)
+         ET.SubElement(doc, "field6", name="Filtered").text = str(filteredp)
 
          tree = ET.ElementTree(root)
          tree.write(opslaan)
 
       #Naar Json schrijven
       elif keuze == "2":
-         opslaan = input("Hoe wilt u het bestand noemen?: ") + ".Json"
-         print("De gegevens worden naar een Json bestand geschreven.")
-         json_data_scan = {"Target": ip,
-                           "Scantype": scan,
-                           "Open" : openp ,
-                           "Dicht" : geslotenp,
-                           "Filtered" : filteredp,
-                        }
-         with open(opslaan, 'w') as f:
-            json.dump(json_data_scan, f)
+          opslaan = input("Hoe wilt u het bestand noemen?: ") + ".json"
+          print("De gegevens worden naar een Json bestand geschreven.")
+          json_data_scan = {"Target": ip,
+                            "Scantype": scantype,
+                            "Port Range" : f"{porta}-{portb}",
+                            "Open": openp,
+                            "Dicht": geslotenp,
+                            "Filtered": filteredp
+                            }
+          with open(opslaan, 'w') as f:
+              json.dump(json_data_scan, f)
 
       else:
          print("Dit is geen geldige keuze.")
